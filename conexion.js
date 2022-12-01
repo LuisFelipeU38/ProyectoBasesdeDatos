f399a557b06d9ad70acc9285a4e7f7e049a4d98b
LuisFelipeU38 <83362726+LuisFelipeU38@users.noreply.github.com>
2022-12-01 14:54:22 -0500

    Update conexion.js

--- conexion.js
+++ conexion.js
@@ -1,8 +1,8 @@
 const mysql = require("mysql");
 // Coloca aquí tus credenciales
 module.exports = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "acuario77",
+  host: "",
+  user: "",
+  password: "",
   database: "tienda"
 });
